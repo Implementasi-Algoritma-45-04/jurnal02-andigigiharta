d7bb54ad7f0df69bae5a1817a0b520c4b09645b1
andigigiharta <andigigihartadwiputra@gmail.com>
2021-10-01 16:26:54 +0700

    solusi Jurnal 02

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02 {
+	public static void main(String[] args) {
+		System.out.println("NIM  : 6706210069");
+		System.out.println("Nama : Andi Gigih Artadwiputra");
+		System.out.println("Kelas: D3IF4504");
+	}
+} 
